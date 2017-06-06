1f31649396f19b5d91577972f4718b36d77c64b7
Eric Marchand <eric.marchand@4d.com>
2017-06-06 15:16:21 +0100

    [Mobile] start implementing data store corruption test
    Imported from Git
     Author: Eric Marchand <eric.marchand@4d.com> 1496758574 +0200
     Committer: Eric Marchand <eric.marchand@4d.com> 1496758574 +0200
     sha1: 10c93370e8fff3fce4f36f05297ab84c0a91fb14
     push-state: complete
     parent-changes: a3b27689ee2bf0873e0e7ae436e4f38c65988709=[212141]
    
    [git-p4: depot-paths = "//depot/4eDimension/main/QMobile/QMobileDataStore/": change = 212517]

---
+++ Tests/CoreDataStoreCorrupedTests.swift
@@ -0,0 +1,121 @@
+//
+//  CoreDataStoreCorrupedTests.swift
+//  QMobileDataStore
+//
+//  Created by Eric Marchand on 25/04/2017.
+//  Copyright © 2017 Eric Marchand. All rights reserved.
+//
+
+import Foundation
+
+import XCTest
+@testable import QMobileDataStore
+
+import Prephirences
+import Result
+import CoreData
+
+class CoreDataStoreCorrupedTests: XCTestCase {
+    
+    let bundle = Bundle(for: CoreDataStoreCorrupedTests.self)
+    
+    let timeout: TimeInterval = 10
+    
+    let table = "Entity"
+    
+    let waitHandler: XCWaitCompletionHandler = { error in
+        if let error = error {
+            XCTFail("Failed to wait expectation: \(error)")
+        }
+    }
+    
+    override func setUp() {
+        super.setUp()
+        Bundle.dataStore = bundle
+        XCTAssertNotNil(Bundle.dataStoreModelName)
+        print("\(dataStore)")
+        
+
+    }
+    
+    override func tearDown() {
+        let expectation = self.expectation(description: #function)
+        
+        
+        dataStore.drop { result in
+            switch result {
+            case .success:
+                expectation.fulfill()
+            case .failure(let error):
+                
+                XCTFail("\(error)")
+            }
+        }
+        self.waitForExpectations(timeout: timeout, handler: waitHandler)
+        
+        super.tearDown()
+    }
+
+    func _testDataStoreCorrupted() {
+        let expectation = self.expectation(description: #function)
+        
+        (dataStore as? CoreDataStore)?.corrupt()
+        
+        dataStore.load { result in
+            
+            switch result {
+            case .success:
+                expectation.fulfill()
+            case .failure(let error):
+                XCTFail("\(error)")
+            }
+        }
+        self.waitForExpectations(timeout: timeout, handler: waitHandler)
+    }
+    
+    
+}
+
+
+extension CoreDataStore {
+    func corrupt() {
+        if let storeURL = self.storeURL {
+            storeURL.corrupt()
+            URL(fileURLWithPath:  "\(storeURL.absoluteString)-shm").corrupt()
+            URL(fileURLWithPath:  "\(storeURL.absoluteString)-wal").corrupt()
+        }
+    }
+}
+
+extension URL {
+    
+    func corrupt() {
+        if !self.isFileURL {
+            return
+        }
+        do {
+            //touch()
+            let txt = UUID().uuidString
+            try txt.write(toFile: self.absoluteString, atomically: true, encoding: .utf8)
+        } catch {
+            XCTFail("\(error)")
+        }
+    }
+    
+    func touch() {
+        if !self.exists {
+            createFile()
+        }
+    }
+    
+    func createFile()   {
+        if !FileManager.default.createFile(atPath: self.absoluteString, contents: nil, attributes: nil) {
+            
+            XCTFail("cannot create \(self.absoluteString)")
+        }
+    }
+    
+    var exists: Bool {
+        return FileManager.default.fileExists(atPath: self.absoluteString)
+    }
+}
